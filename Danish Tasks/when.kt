40b59cf137badf1931d43d1ace0f567c92c653e7
Danishkhan8866 <164178583+Danishkhan8866@users.noreply.github.com>
2024-03-27 17:55:40 +0530

    Add files via upload

---
+++ Danish Tasks/when.kt
@@ -0,0 +1,20 @@
+fun main() {
+  var month = 1
+
+  var year = when (month) {
+    1 -> "January"
+    2 -> "February"
+    3 -> "March"
+    4 -> "April"
+    5 -> "May"
+    6 -> "June"
+    7 -> "July"
+    8 -> "August"
+    9 -> "September"
+    10 -> "October"
+    11 -> "November"
+    12 -> "December"
+    else -> "End of the Year"
+  }
+  println(year)
+}
