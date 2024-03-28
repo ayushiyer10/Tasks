9ce5d19e8d48866f42105c07a588d9509cd5c201
Danishkhan8866 <164178583+Danishkhan8866@users.noreply.github.com>
2024-03-28 17:32:17 +0530

    Add files via upload

---
+++ Danish Tasks/break.kt
@@ -0,0 +1,27 @@
+//Break using while loop 
+
+fun main() {
+    var i = 1
+    while (i < 10) {
+      println(i)
+      i++
+      if (i == 9) {
+        break
+      }
+    }
+  }
+
+  // Break using for loop.
+  fun main(){
+    for (t in (1..10)){
+        
+        println(t)
+        if(t==8){
+            break
+        }
+    }
+        
+  }
+  
+
+  
