417ffcb4043c45621e5b8dfb2c6b6725d9209d03
Danishkhan8866 <164178583+Danishkhan8866@users.noreply.github.com>
2024-03-28 17:33:38 +0530

    Add files via upload

---
+++ Danish Tasks/nestedloop.kt
@@ -0,0 +1,14 @@
+fun main() {
+    var rows = 5
+    var columns = 5
+
+    for (i in 0 until rows) {
+        
+        for (j in 0 until columns) {
+            print("* ")
+        }
+        
+        println()
+    }
+}
+
